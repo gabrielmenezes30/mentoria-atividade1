ee9c4e26d7e4463721c8e91f50390e251c81c4a8
Heldersilva959 <heldersilva959@gmail.com>
2024-12-18 18:23:22 -0300

    Adição das funções de comprimento e massa

--- exercicios/conversoes.c
+++ exercicios/conversoes.c
@@ -29,3 +29,141 @@ int main(){
     }
     
 }
+
+int comprimento() {
+printf("============CONVERSOR DE UNIDADE DE COMPRIMENTO============\n");
+printf("Informe o valor a ser convertido: \n");
+float num=0;
+scanf("%f", &num);
+printf("\nInforme a medida de origem: \n1-Metro\n2-Centimetro\n3-Milimetro\n");
+int origem=0;
+scanf("%d", &origem);
+printf("\nInforme a medida de destino: \n1-Metro\n2-Centimetro\n3-Milimetro \n");
+int destino=0;
+scanf("%d", &destino);
+
+switch (origem)
+{
+case 1:
+    if(destino == 1){
+        printf("\nValor já convertido");
+    }
+    else if(destino == 2){
+        num = num*100;
+        printf("\nValor convertido: %.2f",num);
+    }
+    else if(destino == 3){
+        num = num*1000;
+        printf("\nValor convertido: %.2f", num);
+    }
+    else{
+        printf("\nValor de destino invalido");
+    }
+    break;
+case 2:
+    if(destino == 1){
+        num = num*0.01;
+        printf("\nValor convertido: %.2f", num);
+    }
+    else if(destino == 2){
+         printf("\nValor já convertido");
+    }
+    else if(destino == 3){
+        num = num*10;
+        printf("\nValor convertido: %.2f", num);
+    }
+    else{
+        printf("\nValor de destino invalido");
+    }
+break;
+case 3:
+    if(destino == 1){
+        num = num*0.001;
+        printf("\nValor convertido: %.2f", num); 
+    }
+    else if(destino==2){
+        num = num*0.1;
+        printf("\nValor convertido: %.2f", num);
+    }
+    else if(destino == 3){
+         printf("\nValor já convertido");
+    }
+    else{
+        printf("\nValor de destino invalido");
+    }
+default:
+    printf("\nValor de origem invalido");
+    break;
+}
+
+return 0;
+}
+
+int massa(){
+    printf("======CONVERSOR DE UNIDADE DE MASSA======\n");
+    printf("Informe o peso a ser convertido: \n");
+    float num=0;
+    scanf("%f", &num);
+    printf("\nInforme a medida de origem: \n1-Quilograma\n2-Grama\n3-Tonelada\n");
+    int origem=0;
+    scanf("%d", &origem);
+    printf("\nINFORME A MEDIDA DE DESTINO: \n1-Quilograma\n2-Grama\n3-Tonelada\n");
+    int destino=0;
+    scanf("%d", &destino);
+
+    switch (origem)
+    {
+    case 1:
+        if(destino == 1){
+            printf("Valor ja convertido");
+        }
+        else if(destino == 2){
+            num = num * 1000;
+             printf("Valor convertido: %.2f", num);
+        }
+        else if(destino == 3){
+            num = num * 0.001;
+             printf("Valor convertido: %.2f", num);
+        }
+        else{
+            printf("Valor de destino invalido");
+        }
+        break;
+    case 2:
+        if(destino == 1){
+            num = num*0.001;
+             printf("Valor convertido: %.2f", num);
+        }
+        else if(destino == 2){
+            printf("Valor ja convertido");
+        }
+        else if(destino == 3){
+            num = num*0.000001;
+             printf("Valor convertido: %.2f", num);
+        }
+        else{
+           printf("Valor de destino invalido"); 
+        }
+        break;
+    case 3:
+     if(destino == 1){
+            num = num*1000;
+             printf("Valor convertido: %.2f", num);
+        }
+    else if(destino == 2){
+        num = num*1000000;
+        printf("Valor convertido: %.2f", num);
+    }
+    else if(destino == 3){
+         printf("Valor ja convertido");
+    }
+    else{
+        printf("Valor de destino invalido");   
+    }
+    break;
+    default:
+         printf("Valor de origem invalido"); 
+        break;
+    }
+   return 0;
+}
--- questao.c
+++ questao.c
@@ -5,15 +5,19 @@ int main(){
     printf("ESCOLHA O TIPO DE CONVERSAO\n1-COMPRIMENTO\n2-MASSA\n");
     int tipo =0;
     scanf("%d", &tipo);
+    // Estrutura de seleção para executar a conversão com base na escolha do usuário
     switch (tipo)
     {
     case 1:
+    // Chama a função para conversão de comprimento
         comprimento();
         break;
     case 2: 
+        // Chama a função para conversão de massa
          massa();
          break;
     default:
+     // Caso o usuário insira uma opção inválida
         printf("OPCAO INVALIDA");
         break;
     }
@@ -90,73 +94,74 @@ default:
 return 0;
 }
 
-int massa() {
-printf("============CONVERSOR DE UNIDADE DE COMPRIMENTO============\n");
-printf("INFORME O VALOR A SER CONVERTIDO: \n");
-float num=0;
-scanf("%f", &num);
-printf("\nINFORME A MEDIDA DE ORIGEM: \n1-METRO\n2-CENTIMETRO\n3-MILIMETRO\n");
-int origem=0;
-scanf("%d", &origem);
-printf("\nINFORME A MEDIDA DE DESTINO:\n1-METRO\n2-CENTIMETRO\n3-MILIMETRO \n");
-int destino=0;
-scanf("%d", &destino);
 
-switch (origem)
-{
-case 1:
-    if(destino == 1){
-        printf("\nValor ja convertido");
-    }
-    else if(destino == 2){
-        num = num*100;
-        printf("\nValor convertido: %.2f",num);
-    }
-    else if(destino == 3){
-        num = num*1000;
-        printf("\nValor convertido: %.2f", num);
-    }
-    else{
-        printf("\nValor de destino invalido");
-    }
-    break;
-case 2:
-    if(destino == 1){
-        num = num*0.01;
-        printf("\nValor convertido: %.2f", num);
-    }
-    else if(destino == 2){
-         printf("\nValor ja convertido");
-    }
-    else if(destino == 3){
-        num = num*10;
-        printf("\nValor convertido: %.2f", num);
-    }
-    else{
-        printf("\nValor de destino invalido");
-    }
-break;
-case 3:
-    if(destino == 1){
-        num = num*0.001;
-        printf("\nValor convertido: %.2f", num); 
-    }
-    else if(destino==2){
-        num = num*0.1;
-        printf("\nValor convertido: %.2f", num);
-    }
-    else if(destino == 3){
-         printf("\nValor ja convertido");
-    }
-    else{
-        printf("\nValor de destino invalido");
-    }
-    break;
-default:
-    printf("\nValor de origem invalido");
-    break;
-}
 
-return 0;
+int massa(){
+    printf("======CONVERSOR DE UNIDADE DE MASSA======\n");
+    printf("INFORME O PESO A SER CONVERTIDO: \n");
+    float num=0;
+    scanf("%f", &num);
+    printf("\nINFORME A MEDIDA DE ORIGEM: \n1-QUILOGRAMA\n2-GRAMA\n3-TONELADA\n");
+    int origem=0;
+    scanf("%d", &origem);
+    printf("\nINFORME A MEDIDA DE DESTINO: \n1-QUILOGRAMA\n2-GRAMA\n3-TONELADA\n");
+    int destino=0;
+    scanf("%d", &destino);
+
+    switch (origem)
+    {
+    case 1:
+        if(destino == 1){
+            printf("VALOR JA CONVERTIDO");
+        }
+        else if(destino == 2){
+            num = num * 1000;
+            printf("VALOR CONVERTIDO: %.2f", num);
+        }
+        else if(destino == 3){
+            num = num * 0.001;
+            printf("VALOR CONVERTIDO: %.2f", num);
+        }
+        else{
+            printf("VALOR DE DESTINO INVALIDO");
+        }
+        break;
+    case 2:
+        if(destino == 1){
+            num = num*0.001;
+            printf("VALOR CONVERTIDO: %.2f", num);
+        }
+        else if(destino == 2){
+            printf("VALOR JA CONVERTIDO");
+        }
+        else if(destino == 3){
+            num = num*0.000001;
+            printf("VALOR CONVERTIDO: %.2f", num);
+        }
+        else{
+           printf("VALOR DE DESTINO INVALIDO"); 
+        }
+        break;
+    case 3:
+     if(destino == 1){
+            num = num*1000;
+            printf("VALOR CONVERTIDO: %.2f", num);
+        }
+    else if(destino == 2){
+        num = num*1000000;
+        printf("VALOR CONVERTIDO: %.2f", num);
+    }
+    else if(destino == 3){
+         printf("VALOR JA CONVERTIDO");
+    }
+    else{
+        printf("VALOR DE DESTINO INVALIDO");   
+    }
+    break;
+    default:
+         printf("VALOR DE ORIGEM INVALIDO"); 
+        break;
+    }
+   return 0;
 }
 
